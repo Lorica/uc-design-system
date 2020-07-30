3b2a6d8c3cc8bf9c08e7cb9781c2cf965e7cf0a2
Orlando Del Aguila <orlando@hashlabs.com>
2020-07-30 18:17:05 -0500

    dev: fix import path in storybook plugin

--- .storybook/addons/props/getImportPath.js
+++ .storybook/addons/props/getImportPath.js
@@ -1,44 +1,39 @@
-export default function getImportPath(filePath, baseName) {
-  const indexParts = filePath.match(
-    /^packages\/([\w-]+)\/src\/index\.tsx?#(\w+)$/i
-  )
+export default function getImportPath (filePath, baseName) {
+  const indexParts = filePath.match(/^packages\/([\w-]+)\/src\/index\.tsx?#(\w+)$/i);
 
   if (indexParts) {
-    const [, packageName, compName] = indexParts
+    const [, packageName, compName] = indexParts;
 
-    return `import ${compName} from '@airbnb/${packageName}';`
+    return `import ${compName} from '@lorica/${packageName}';`;
   }
 
-  const parts = filePath.match(
-    /^packages\/([\w-]+)\/src\/components\/(\w+)(\/\w+)?\.tsx#(\w+)$/i
-  )
+  const parts = filePath.match(/^packages\/([\w-]+)\/src\/components\/(\w+)(\/\w+)?\.tsx#(\w+)$/i);
 
   if (!parts) {
-    return null
+    return null;
   }
 
-  const [, pkgName, compName, baseChildName] = parts
-  const packageName =
-    pkgName === 'core' ? 'uc-design-system' : `uc-design-system-${pkgName}`
-  const childName = baseChildName ? baseChildName.slice(1) : null
+  const [, pkgName, compName, baseChildName] = parts;
+  const packageName = pkgName === 'core' ? 'uc-design-system' : `uc-design-system-${pkgName}`;
+  const childName = baseChildName ? baseChildName.slice(1) : null;
 
   // Composer
   if (packageName === 'uc-design-system-composer') {
     if (compName === 'Composer') {
-      return `import ${compName} from '@airbnb/${packageName}'`
+      return `import ${compName} from '@lorica/${packageName}'`;
     }
 
-    return `import { ${compName || childName} } from '@airbnb/${packageName}'`
+    return `import { ${compName || childName} } from '@lorica/${packageName}'`;
   }
 
   // Forms
   if (compName === 'Form') {
-    return `import ${compName} from '@airbnb/${packageName}'`
+    return `import ${compName} from '@lorica/${packageName}'`;
   }
 
   if (!childName || childName === 'index') {
-    return `import ${compName} from '@airbnb/${packageName}/lib/components/${compName}'`
+    return `import ${compName} from '@lorica/${packageName}/lib/components/${compName}'`;
   }
 
-  return `import { ${childName} } from '@airbnb/${packageName}/lib/components/${compName}'`
+  return `import { ${childName} } from '@lorica/${packageName}/lib/components/${compName}'`;
 }
